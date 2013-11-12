1041806368e5b21743ef070fbd8a5b811d25e335
NFM <jwj0831@gmail.com>
2013-11-12 22:32:27 +0900

    modify script location

--- index.php
+++ index.php
@@ -30,54 +30,6 @@ $total_pages = ceil($get_total_rows[0] / $item_per_page);
 		<!-- Custom styles for this template -->
 		<link rel="stylesheet" href="./css/dust-portal.css" >
 	
-		<script>
-			$(document).ready(function() {
-
-    			var track_click = 0; //track user click on "load more" button, righ now it is 0 click
-    
-    			var total_pages = <?php echo $total_pages; ?>;
-
-				$('#results').load("fetch_pages.php", {'page' : track_click}, function() { track_click++;}); //initial data to load
-
-				$(".load_more").click(function(e) { //user clicks on button
-
-					$(this).hide(); //hide load more button on click
-					$('.animation_image').show(); //show loading image
-
-					if (track_click <= total_pages)//user click number is still less than total pages
-					{
-						//post page number and load returned data into result element
-						$.post('fetch_pages.php', {'page' : track_click}, function(data) {
-
-							$(".load_more").show();//bring back load more button
-
-							$("#results").append(data);//append data received from server
-
-							//scroll page smoothly to button id
-							$("html, body").animate({scrollTop : $("#load_more_button").offset().top}, 500);
-
-							//hide loading image
-							$('.animation_image').hide(); //hide loading image once data is received
-			
-							track_click++; //user click increment on load button
-
-					}).fail(function(xhr, ajaxOptions, thrownError) {//any errors?
-						alert(thrownError); //alert with HTTP error
-						$(".load_more").show(); //bring back load more button
-						$('.animation_image').hide();//hide loading image once data is received
-					});
-
-					if (track_click >= total_pages - 1)//compare user click with page number
-					{
-						//reached end of the page yet? disable load button
-						$(".load_more").attr("disabled", "disabled");
-					}
-				}
-
-			});
-		});
-		</script>
-	
 		<!-- Just for debugging purposes. Don't actually copy this line! -->
 		<!--[if lt IE 9]><script src="../../docs-assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
 
@@ -125,5 +77,53 @@ $total_pages = ceil($get_total_rows[0] / $item_per_page);
 		<!-- Placed at the end of the document so the pages load faster -->
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 		<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.1/js/bootstrap.min.js"></script>
+		
+				<script>
+			$(document).ready(function() {
+
+    			var track_click = 0; //track user click on "load more" button, righ now it is 0 click
+    
+    			var total_pages = <?php echo $total_pages; ?>;
+
+				$('#results').load("fetch_pages.php", {'page' : track_click}, function() { track_click++;}); //initial data to load
+
+				$(".load_more").click(function(e) { //user clicks on button
+
+					$(this).hide(); //hide load more button on click
+					$('.animation_image').show(); //show loading image
+
+					if (track_click <= total_pages)//user click number is still less than total pages
+					{
+						//post page number and load returned data into result element
+						$.post('fetch_pages.php', {'page' : track_click}, function(data) {
+
+							$(".load_more").show();//bring back load more button
+
+							$("#results").append(data);//append data received from server
+
+							//scroll page smoothly to button id
+							$("html, body").animate({scrollTop : $("#load_more_button").offset().top}, 500);
+
+							//hide loading image
+							$('.animation_image').hide(); //hide loading image once data is received
+			
+							track_click++; //user click increment on load button
+
+					}).fail(function(xhr, ajaxOptions, thrownError) {//any errors?
+						alert(thrownError); //alert with HTTP error
+						$(".load_more").show(); //bring back load more button
+						$('.animation_image').hide();//hide loading image once data is received
+					});
+
+					if (track_click >= total_pages - 1)//compare user click with page number
+					{
+						//reached end of the page yet? disable load button
+						$(".load_more").attr("disabled", "disabled");
+					}
+				}
+
+			});
+		});
+		</script>
 	</body>
 </html>
