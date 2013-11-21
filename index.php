67e3bca1f89ae9e5911ccb673890e0b77e04bb7a
NFM <jwj0831@gmail.com>
2013-11-22 04:51:51 +0900

    update css

--- index.php
+++ index.php
@@ -1,10 +1,28 @@
 <?php
 include ("config.inc.php");
 
-$results = $mysqli -> query("SELECT COUNT(*) as t_records FROM dust_val");
-$total_records = $results -> fetch_object();
-$total_groups = ceil($total_records -> t_records / $items_per_group);
-$results -> close();
+$result = $mysqli->query("SELECT raw_data, idi_data FROM dust_data ORDER BY id DESC LIMIT 0, 1");
+$obj = $result->fetch_object();
+
+$raw = $obj->raw_data;
+$idi = $obj->idi_data;
+$idi_string = "";
+switch($idi){
+	case 0:
+		$idi_string = "Good";
+		break;
+	case 1:
+		$idi_string = "Not Bad";
+		break;
+	case 2:
+		$idi_string = "Severe";
+		break;
+}
+
+$result->close();
+
+//$time = strtotime($dateInUTC.'KST');
+//$dateInLocal = date("Y-m-d H:i:s",$time);
 ?>
 
 <!DOCTYPE html>
@@ -12,13 +30,14 @@ $results -> close();
 	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<<meta http-equiv='refresh' content='30;url=http://117.16.146.81/dust-portal'>
+		<meta http-equiv='refresh' content='30;url=http://117.16.146.81/dust-portal'>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="description" content="">
 		<meta name="author" content="">
-		<link rel="shortcut icon" href="../../docs-assets/ico/favicon.png">
+		<link rel="shortcut icon" href="./static/favicon.ico" type="image/x-icon">
+		<link rel="icon" href="./static/favicon.ico" type="image/x-icon">
 
-		<title>Real time Dust Monitoring System</title>
+		<title>Indoor Dust Monitor</title>
 
 		<!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.1/css/bootstrap.min.css">
@@ -27,7 +46,7 @@ $results -> close();
 		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.1/css/bootstrap-theme.min.css">
 
 		<!-- Custom styles for this template -->
-		<link rel="stylesheet" href="./css/dust-portal.css" >
+		<link rel="stylesheet" href="./static/css/dust-portal.css" >
 
 		<!-- Just for debugging purposes. Don't actually copy this line! -->
 		<!--[if lt IE 9]><script src="../../docs-assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
@@ -40,86 +59,69 @@ $results -> close();
 	</head>
 
 	<body>
-		<nav class="navbar navbar-default navbar-static-top" role="navigation">
-			<a class="navbar-brand" href="http://117.16.146.55/dust-portal">Real Time Dust Monitoring System</a>
-		</nav>
-
 		<div class="container">
-
-			<div class="dust-template">
-				<p class="lead">
-					제 연구실 내 실시간 공기질을 모니터링하는 페이지 입니다.
-				</p>
-
-				<div class="panel panel-default">
-					<div class="panel-heading">
-						Dust Value
-					</div>
-					<div class="panel-body">
-						30초마다 먼지센서로부터 값을 받아서 보여주고 있습니다.</br>
-						개발과정 다음을 참조해 주세요</br>
-					</div>
-
+			<div class="header">
+				<h3 class="text-muted text-center"><a class="title" href="http://117.16.146.81/dust-portal/dust.php"><strong>Real-time Indoor Dust Monitoring System</strong></a></h3>
+				<!--
+				<ul class="nav nav-pills pull-right">
+					<li><a id="home" class="active" href="#">Home</a></li>
+					<li><a id="settings"href="#">Settings</a></li>
+					<li><a id="about" href="#">About</a></li>
+				</ul>
+				-->
+			</div>
+			
+			<div id="top-row" class="row">
+				<div id="symbol-info" class="grid-block col-md-6 col-lg-6 text-center">
+						<span id="symbol-label" class="grid-label">Indoor</br> Dust Index</span></br>
+						<strong><span id="symbol-val"><?php echo $idi_string; ?></span></strong>
+				</div>
+				<div id="time-info" class="grid-block col-md-6 col-lg-6 text-center">
+						<span id="clock-label" class="grid-label">Time</span></br>
+						<strong><span id="clock"></span></strong>
+				</div>
+			</div>
+			<div class="row">
+				<div id="max-info" class="grid-block col-md-6 col-lg-6 text-center">
+						<span id="max-label" class="grid-label">Today's</br> Max Value</span></br>
+						<strong><span id="max-val">2555</span></strong>
 					
-					<div id="results">
-					</div>
-					<div class="animation_image" style="display:none" align="center">
-						<img src="ajax-loader.gif">
-					</div>
-				</div><!-- /.panel -->
-			</div><!-- /.dust-template -->
+				</div>
+				<div id="dust-info" class="grid-block col-md-6 col-lg-6 text-center">
+						<span id="dust-label" class="grid-label">Current</br> Dust Value</span></br>
+						<strong><span id="dust-val"><?php echo $raw;?></span></strong>
+				</div>
+			</div>
+			
+			<div class="footer text-center">Copyright at <strong>K2V</strong> in 2013 Fusion Project Class</div>
 		</div><!-- /.container -->
-		<footer>
-			<p class="footer">
-				Copyright at NFM in 2013
-			</p>
-		</footer>
+
 		<!-- Bootstrap core JavaScript
 		================================================== -->
 		<!-- Placed at the end of the document so the pages load faster -->
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
 		<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.1/js/bootstrap.min.js"></script>
-
+		<script src="//ajax.googleapis.com/ajax/libs/prototype/1.7.1.0/prototype.js"></script>
+		<script type="text/javascript"> 
+			new Ajax.PeriodicalUpdater('clock', 'clock.php', {method: 'get', frequency: 1 });
+		</script>
 		<script>
 			$(document).ready(function() {
-				var track_load = 0; //total loaded record group(s)
-				var loading  = false; //to prevents multipal ajax loads
-				var total_groups = <?php echo $total_groups; ?>;//total record group(s)
-
-				$('#results').load("autoload_process.php", {'group_no' : track_load}, function() {track_load++;}); //load first group
-
-				$(window).scroll(function() {//detect page scroll
-					if ($(window).scrollTop() + $(window).height() == $(document).height())//user scrolled to bottom of the page?
-					{
-						if (track_load <= total_groups && loading == false)//there's more data to load
-						{
-							loading = true;	//prevent further ajax loading
-							$('.animation_image').show(); //show loading image
-
-							//load data from the server using a HTTP POST request
-							$.post('autoload_process.php', {'group_no' : track_load}, function(data) {
-
-								$("#results").append(data);//append received data into the element
-
-								//hide loading image
-								$('.animation_image').hide();//hide loading image once data is received
-
-								track_load++;
-								//loaded group increment
-								loading = false;
-
-							}).fail(function(xhr, ajaxOptions, thrownError) {//any errors?
-
-								alert(thrownError);//alert with HTTP error
-								$('.animation_image').hide();//hide loading image
-								loading = false;
-
-							});
-	
-						}
-					}
-				});
-			});
+				var idi = <?php echo $idi; ?>;// idi_number;
+				switch (idi)
+				{
+				case 0:
+					$("#symbol-info")..css( "background-color", "#f5bb63" );
+				  	break;
+				case 1:
+				 	$("#symbol-info")..css( "background-color", "#e8703e" );
+				  	break;
+				 case 2:
+				 	$("#symbol-info")..css( "background-color", "#f5bb63z" );
+				  	break;
+				default:
+				  $("#symbol-info")..css( "background-color", "#f5bb63" );
+				}
 		</script>
 	</body>
 </html>
---
+++ index_back.php
@@ -0,0 +1,125 @@
+<?php
+include ("config.inc.php");
+
+$results = $mysqli -> query("SELECT COUNT(*) as t_records FROM dust_val");
+$total_records = $results -> fetch_object();
+$total_groups = ceil($total_records -> t_records / $items_per_group);
+$results -> close();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<<meta http-equiv='refresh' content='30;url=http://117.16.146.81/dust-portal'>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<link rel="shortcut icon" href="../../docs-assets/ico/favicon.png">
+
+		<title>Real time Dust Monitoring System</title>
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.1/css/bootstrap.min.css">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.1/css/bootstrap-theme.min.css">
+
+		<!-- Custom styles for this template -->
+		<link rel="stylesheet" href="./css/dust-portal.css" >
+
+		<!-- Just for debugging purposes. Don't actually copy this line! -->
+		<!--[if lt IE 9]><script src="../../docs-assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+		<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+		<script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+		<![endif]-->
+	</head>
+
+	<body>
+		<nav class="navbar navbar-default navbar-static-top" role="navigation">
+			<a class="navbar-brand" href="http://117.16.146.55/dust-portal">Real Time Dust Monitoring System</a>
+		</nav>
+
+		<div class="container">
+
+			<div class="dust-template">
+				<p class="lead">
+					제 연구실 내 실시간 공기질을 모니터링하는 페이지 입니다.
+				</p>
+
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						Dust Value
+					</div>
+					<div class="panel-body">
+						30초마다 먼지센서로부터 값을 받아서 보여주고 있습니다.</br>
+						개발과정 다음을 참조해 주세요</br>
+					</div>
+
+					
+					<div id="results">
+					</div>
+					<div class="animation_image" style="display:none" align="center">
+						<img src="ajax-loader.gif">
+					</div>
+				</div><!-- /.panel -->
+			</div><!-- /.dust-template -->
+		</div><!-- /.container -->
+		<footer>
+			<p class="footer">
+				Copyright at NFM in 2013
+			</p>
+		</footer>
+		<!-- Bootstrap core JavaScript
+		================================================== -->
+		<!-- Placed at the end of the document so the pages load faster -->
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+		<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.1/js/bootstrap.min.js"></script>
+
+		<script>
+			$(document).ready(function() {
+				var track_load = 0; //total loaded record group(s)
+				var loading  = false; //to prevents multipal ajax loads
+				var total_groups = <?php echo $total_groups; ?>;//total record group(s)
+
+				$('#results').load("autoload_process.php", {'group_no' : track_load}, function() {track_load++;}); //load first group
+
+				$(window).scroll(function() {//detect page scroll
+					if ($(window).scrollTop() + $(window).height() == $(document).height())//user scrolled to bottom of the page?
+					{
+						if (track_load <= total_groups && loading == false)//there's more data to load
+						{
+							loading = true;	//prevent further ajax loading
+							$('.animation_image').show(); //show loading image
+
+							//load data from the server using a HTTP POST request
+							$.post('autoload_process.php', {'group_no' : track_load}, function(data) {
+
+								$("#results").append(data);//append received data into the element
+
+								//hide loading image
+								$('.animation_image').hide();//hide loading image once data is received
+
+								track_load++;
+								//loaded group increment
+								loading = false;
+
+							}).fail(function(xhr, ajaxOptions, thrownError) {//any errors?
+
+								alert(thrownError);//alert with HTTP error
+								$('.animation_image').hide();//hide loading image
+								loading = false;
+
+							});
+	
+						}
+					}
+				});
+			});
+		</script>
+	</body>
+</html>
