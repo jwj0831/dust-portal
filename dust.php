ef40d15eb29ad18e5c21eb5e4279a02aa7679b4b
NFM <jwj0831@gmail.com>
2013-11-21 19:18:30 +0900

    update

--- dust.php
+++ dust.php
@@ -80,7 +80,7 @@ $results -> close();
 		<!-- Bootstrap core JavaScript
 		================================================== -->
 		<!-- Placed at the end of the document so the pages load faster -->
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
 		<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.1/js/bootstrap.min.js"></script>
 	</body>
 </html>
