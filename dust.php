ab9e33165d9e7da2945d628f37db523371362e47
NFM <jwj0831@gmail.com>
2013-11-21 20:46:32 +0900

    local time

--- dust.php
+++ dust.php
@@ -6,8 +6,8 @@ $total_records = $results -> fetch_object();
 $total_groups = ceil($total_records -> t_records / $items_per_group);
 $results -> close();
 
-//$dt = new DateTime();
-
+$time = strtotime($dateInUTC.'UTC');
+$dateInLocal = date("Y-m-d H:i:s",$time);
 ?>
 
 <!DOCTYPE html>
@@ -63,7 +63,7 @@ $results -> close();
 				</div>
 				<div id="text-info" class="col-md-6 col-lg-6">
 					<div id="today-time">
-						<p class="text-primary text-val"><strong>Current Time: <?php echo date('Y-m-d H:i:s'); ?></strong></p>
+						<p class="text-primary text-val"><strong>Current Time: <?php echo $dateInLocal; ?></strong></p>
 					</div>
 					<div id="today-max">
 						<p class="text-danger text-val"><strong>Today's Max Value: ...</strong></p>
