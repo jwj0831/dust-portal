02dc2619cfc8285e34a72593123b58017b1cb695
NFM <jwj0831@gmail.com>
2013-11-28 17:19:24 +0900

    update form

--- settings.php
+++ settings.php
@@ -60,31 +60,31 @@ $result->close();
 			<form method="post" role="form" action="./setting_update.php">
 				<div class="form-group">
 			    	<label for="lc">Lower Constant: </label>
-			    	<input type="text" class="form-control" id="lc" value="<?php echo $lc;?>">
+			    	<input type="text" class="form-control" id="lc" name="lc" value="<?php echo $lc;?>">
 			  	</div>
 			  	<div class="form-group">
 			    	<label for="lrc">Lower Constant Frequency: </label>
-			    	<input type="text" class="form-control" id="lrc" value="<?php echo $lrc;?>">
+			    	<input type="text" class="form-control" id="lrc" name="lrc" value="<?php echo $lrc;?>">
 			  	</div>
 			  	<div class="form-group">
 			    	<label for="mc">Middle Constant: </label>
-			    	<input type="text" class="form-control" id="mc" value="<?php echo $mc;?>">
+			    	<input type="text" class="form-control" id="mc" name="mc" value="<?php echo $mc;?>">
 			  	</div>
 			  	<div class="form-group">
 			    	<label for="mrc">Middle Constant Frequency: </label>
-			    	<input type="text" class="form-control" id="mrc" value="<?php echo $mrc;?>">
+			    	<input type="text" class="form-control" id="mrc" name="mrc" value="<?php echo $mrc;?>">
 			  	</div>
 			  	<div class="form-group">
 			    	<label for="hc">Higher Constant: </label>
-			    	<input type="text" class="form-control" id="hc" value="<?php echo $hc;?>">
+			    	<input type="text" class="form-control" id="hc" name="hc" value="<?php echo $hc;?>">
 			  	</div>
 			  	<div class="form-group">
 			    	<label for="hrc">Higher Constant Frequency: </label>
-			    	<input type="text" class="form-control" id="hrc" value="<?php echo $hrc;?>">
+			    	<input type="text" class="form-control" id="hrc" name="hrc" value="<?php echo $hrc;?>">
 			  	</div>
 			  	<div class="form-group">
 			    	<label for="window">Window Size: </label>
-			    	<input type="text" class="form-control" id="window" value="<?php echo $window;?>">
+			    	<input type="text" class="form-control" id="window" name="window" value="<?php echo $window;?>">
 			  	</div>
 			 	 <button type="submit" class="btn btn-default">Submit</button>
 			</form>
