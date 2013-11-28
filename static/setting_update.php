eaca2650ed8fc3c4b47f9321dc78b2899627a33b
NFM <jwj0831@gmail.com>
2013-11-28 17:11:33 +0900

    update setting_update part

--- static/setting_update.php
+++ static/setting_update.php
@@ -1,6 +1,18 @@
 <?php
+include ("config.inc.php");
+$lc = $_POST['lc'];
+$lrc = $_POST['lrc'];
+$mc = $_POST['mc'];
+$mrc = $_POST['mrc'];
+$hc = $_POST['hc'];
+$hrc = $_POST['hrc'];
+$window = $_POST['window'];
 
+$sql = $mysqli->prepare("UPDATE dust_conf SET lc = ? lrc = ? mc = ? mrc = ? hc = ? hrc = ? window = ? WHERE id = 1");
+$sql->bind_param('iiiiiii', $lc, $lrc, $mc, $mrc, $hc, $hrc, $window);
+$sql->execute();
+$sql->close();
 
-	header("Location: http://117.16.146.81/dust-portal");
-	exit();
+header("Location: http://117.16.146.81/dust-portal");
+exit();
 ?>
