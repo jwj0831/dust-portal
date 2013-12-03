44e57e5306d9d1e9549b503818be0578a691ca5e
NFM <jwj0831@gmail.com>
2013-12-03 16:35:27 +0900

    1

--- static/js/dust-portal.js
+++ static/js/dust-portal.js
@@ -1,5 +1,6 @@
 var pathArray = window.location.pathname.split( '/' );
 var currentPage = pathArray[2];
+alert(currentPage);
 
 if(currentPage == "list.php") {
 	jQuery("#home").parent().removeClass("active");
@@ -22,7 +23,6 @@ else if(currentPage == "settings.php") {
 else{
 	//Prototype js
 	new Ajax.PeriodicalUpdater('clock', 'clock.php', {method: 'get', frequency: 1 });
-				
 	jQuery("#home").parent().addClass("active");
 	jQuery("#list").parent().removeClass("active");
 	jQuery("#chart").parent().removeClass("active");
