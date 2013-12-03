1988e060838b4ac8158e38f07d248aeb681c6fdc
NFM <jwj0831@gmail.com>
2013-12-03 15:51:44 +0900

    update

--- static/js/nav.js
+++ static/js/nav.js
@@ -1,20 +1,17 @@
 var pathArray = window.location.pathname.split( '/' );
-alert(window.location.pathname);
-var currentPage = pathArray[2];
-alert(currentPage);
 
 if(currentPage == "charts.php") {
-	$("#home").parent().removeClass("active");
-	$("#charts").parent().addClass("active");
-	$("#settings").parent().removeClass("active");
+	jQuery("#home").parent().removeClass("active");
+	jQuery("#charts").parent().addClass("active");
+	jQuery("#settings").parent().removeClass("active");
 }
 else if(currentPage == "settings.php") {
-	$("#home").parent().removeClass("active");
-	$("#charts").parent().removeClass("active");
-	$("#settings").parent().addClass("active");
+	jQuery("#home").parent().removeClass("active");
+	jQuery("#charts").parent().removeClass("active");
+	jQuery("#settings").parent().addClass("active");
 }
 else{
-	$("#home").parent().addClass("active");
-	$("#charts").parent().removeClass("active");
-	$("#settings").parent().removeClass("active");
+	jQuery("#home").parent().addClass("active");
+	jQuery("#charts").parent().removeClass("active");
+	jQuery("#settings").parent().removeClass("active");
 }
