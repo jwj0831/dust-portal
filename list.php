66b94ded6ac808c14fd6316d4838e7a2b0ac679c
NFM <jwj0831@gmail.com>
2013-12-03 16:39:29 +0900

    1

--- list.php
+++ list.php
@@ -26,23 +26,23 @@ include ("header.php");
 				var loading  = false; //to prevents multipal ajax loads
 				var total_groups = <?php echo $total_groups; ?>;//total record group(s)
 
-				$('#results').load("autoload_process.php", {'group_no' : track_load}, function() {track_load++;}); //load first group
+				jQuery('#results').load("autoload_process.php", {'group_no' : track_load}, function() {track_load++;}); //load first group
 
-				$(window).scroll(function() {//detect page scroll
+				jQuery(window).scroll(function() {//detect page scroll
 					if ($(window).scrollTop() + $(window).height() == $(document).height())//user scrolled to bottom of the page?
 					{
 						if (track_load <= total_groups && loading == false)//there's more data to load
 						{
 							loading = true;	//prevent further ajax loading
-							$('.animation_image').show(); //show loading image
+							jQuery('.animation_image').show(); //show loading image
 
 							//load data from the server using a HTTP POST request
-							$.post('autoload_process.php', {'group_no' : track_load}, function(data) {
+							jQuery.post('autoload_process.php', {'group_no' : track_load}, function(data) {
 
-								$("#results").append(data);//append received data into the element
+								jQuery("#results").append(data);//append received data into the element
 
 								//hide loading image
-								$('.animation_image').hide();//hide loading image once data is received
+								jQuery('.animation_image').hide();//hide loading image once data is received
 
 								track_load++;
 								//loaded group increment
@@ -51,7 +51,7 @@ include ("header.php");
 							}).fail(function(xhr, ajaxOptions, thrownError) {//any errors?
 
 								alert(thrownError);//alert with HTTP error
-								$('.animation_image').hide();//hide loading image
+								jQuery('.animation_image').hide();//hide loading image
 								loading = false;
 
 							});
