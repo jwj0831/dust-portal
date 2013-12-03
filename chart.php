b5c675ed0ce33bd527c6f38b91fd6740bc888c36
NFM <jwj0831@gmail.com>
2013-12-03 19:20:47 +0900

    test

--- chart.php
+++ chart.php
@@ -1,32 +1,30 @@
 <?php
 include ("header.php");
 ?>
-<script>
-	google.load("visualization", "1", { packages: ["corechart"] });
-        google.setOnLoadCallback(drawChart);
-
-        function drawChart() {
-            var jsonData = jQuery.ajax({
-                url: "chart_process.php",
-                dataType: "json",
-                async: false
-            }).responseText;
-
-            var obj = window.JSON.stringify(jsonData);
-            var data = google.visualization.arrayToDataTable(obj);
-
-            var options = {
-                title: 'Raw Dust Value'
-            };
-
-            var chart = new google.visualization.LineChart(
-                        document.getElementById('chart_div'));
-            chart.draw(data, options);
-        }
-</script>
+			<script>
+				google.load(“visualization”, “1″, {packages:["corechart"]});
+				google.setOnLoadCallback(drawChart);
+				function drawChart() {
+						var jsonData = $.ajax({
+						url: “chart_process.php”,
+						dataType:”json”,
+						async: false
+					}).responseText;
+					// Create our data table out of JSON data loaded from server.
+					var data = new google.visualization.DataTable(jsonData);
+					var options = {
+						width: 800, height: 480,
+						title: ‘Company Performance’
+					};
+					var chart = new google.visualization.LineChart(document.getElementById(‘chart_div’));
+					chart.draw(data, options);
+				}
+			</script>
 			<div id="chart_div" style="width: 900px; height: 500px;"></div>
-						
-			<div class="footer text-center">Copyright at <strong>K2V</strong> in 2013 Fusion Project Class</div>
+		
+			<div class="footer text-center">
+				Copyright at <strong>K2V</strong> in 2013 Fusion Project Class
+			</div>
 		</div><!-- /.container -->
 	</body>
 </html>
--- chart_process.php
+++ chart_process.php
@@ -1,11 +1,16 @@
 <?php
+
+echo '{ “cols”: [ {"id":"","label":"year","type":"string"}, {"id":"","label":"sales","type":"number"}, {"id":"","label":"expenses","type":"number"}],
+		“rows”: [ {"c":[{"v":"2001"},{"v":3},{"v":5}]}, {“c”:[{"v":"2002"},{"v":5},{"v":10}]}, {“c”:[{"v":"2003"},{"v":6},{"v":4}]}, {“c”:[{"v":"2004"},{"v":8},{"v":32}]},{“c”:[{"v":"2005"},{"v":3},{"v":56}]}]}';
+
+/*
 include("config.inc.php"); //include config file
 $result = $mysqli->query("SELECT timestamp, raw_data, FROM dust_data ORDER BY id DESC LIMIT 0, 20");
 $rows = $result->fetch_all(MYSQLI_ASSOC);
 
 $data[0] = array('time','dust_cnt');
 $i=1;
-foreach($rows as $row){
+foreach($result->fetch_all(MYSQLI_ASSOC)){
 	$data[$i++] = array($row['timestamp'], doubleval($row['raw_data']));
 }
 
@@ -13,4 +18,6 @@ echo json_encode($data);
 
 $result->free();
 $mysqli->close();
+ 
+ */
 ?>
