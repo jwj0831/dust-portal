1437f4b145dac2ea4dacc2d66330629dbd1ff6cd
NFM <jwj0831@gmail.com>
2013-12-03 17:34:18 +0900

    update

--- chart_process.php
+++ chart_process.php
@@ -1,7 +1,7 @@
 <?php
 include("config.inc.php"); //include config file
 $result = $mysqli->query("SELECT timestamp, raw_data, FROM dust_data ORDER BY id DESC LIMIT 0, 20");
-while($row = $result->fetch_array())
+while($row = $result->fetch_array(MYSQLI_ASSOC))
 {
 	$rows[] = $row;
 }
@@ -9,12 +9,8 @@ while($row = $result->fetch_array())
 $data[0] = array('time','dust_cnt');
 $i=1;
 foreach($rows as $row){
-	$data[$i++] = array($row['timestamp'], $row['raw_data']);
+	$data[$i++] = array($row['timestamp'], doubleval($row['raw_data']));
 }
-
-
-
-
 
 echo json_encode($data);
 
