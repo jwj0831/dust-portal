0e9d2ec9b3fc1e0c6031f5456c41d6885f565972
NFM <jwj0831@gmail.com>
2013-12-03 19:53:31 +0900

    update

--- chart_process.php
+++ chart_process.php
@@ -1,17 +1,18 @@
 <?php
-
+/* Test Codes
 echo '{ "cols": [ {"id":"","label":"year","type":"string"}, {"id":"","label":"sales","type":"number"}, {"id":"","label":"expenses","type":"number"}],
 		"rows": [ {"c":[{"v":"2001"},{"v":3},{"v":5}]}, {"c":[{"v":"2002"},{"v":5},{"v":10}]}, {"c":[{"v":"2003"},{"v":6},{"v":4}]}, {"c":[{"v":"2004"},{"v":8},{"v":32}]},{"c":[{"v":"2005"},{"v":3},{"v":56}]}]}';
+*/
 
-/*
 include("config.inc.php"); //include config file
 $result = $mysqli->query("SELECT timestamp, raw_data, FROM dust_data ORDER BY id DESC LIMIT 0, 20");
 $rows = $result->fetch_all(MYSQLI_ASSOC);
 
-$data[0] = array('time','dust_cnt');
-$i=1;
+$data['cols'][] = array('type' => 'string', 'label' => 'timestamp');
+$data['cols'][] = array('type' => 'number', 'label' => 'raw_data');
+
 foreach($result->fetch_all(MYSQLI_ASSOC)){
-	$data[$i++] = array($row['timestamp'], doubleval($row['raw_data']));
+	$data['rows'][] = array('c' => array( 'v' => $row['timestamp'], 'v' => doubleval($row['raw_data']) ) );
 }
 
 echo json_encode($data);
@@ -19,5 +20,5 @@ echo json_encode($data);
 $result->free();
 $mysqli->close();
  
- */
+ 
 ?>
