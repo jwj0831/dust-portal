1eddbf9930b4debc4bd0728db574db8e838356bf
NFM <jwj0831@gmail.com>
2013-12-03 20:16:13 +0900

    update

--- chart.php
+++ chart.php
@@ -16,13 +16,23 @@ include ("header.php");
 			// Create our data table out of JSON data loaded from server.
 			var data = new google.visualization.DataTable(jsonData);
 			var options = {
-				width: 800, height: 480,
+				width: 100%, height: 100%,
 				title: 'Raw Dust Info'
 			};
 			
 			var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
 			chart.draw(data, options);
 			}
+			
+			function resizeHandler () {
+		        chart.draw(data, options);
+		    }
+		    if (window.addEventListener) {
+		        window.addEventListener('resize', resizeHandler, false);
+		    }
+		    else if (window.attachEvent) {
+		        window.attachEvent('onresize', resizeHandler);
+		    }
 			</script>
 			<div id="chart_div" style="width: 900px; height: 500px;"></div>
 		
