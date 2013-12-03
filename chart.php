3f41364c29760527530ce7be8f52f520319925d1
NFM <jwj0831@gmail.com>
2013-12-03 19:33:24 +0900

    test

--- chart.php
+++ chart.php
@@ -1,22 +1,24 @@
 <?php
 include ("header.php");
 ?>
-			<script type=”text/javascript”>
-			google.load(“visualization”, “1″, {packages:["corechart"]});
+			<script type="text/javascript">
+			google.load("visualization", "1", {packages:["corechart"]});
 			google.setOnLoadCallback(drawChart);
 			function drawChart() {
-			var jsonData = $.ajax({
-			url: “chart_process.php”,
-			dataType:”json”,
-			async: false
-			}).responseText;
+				var jsonData = $.ajax({
+					url: "chart_process.php",
+					dataType:"json",
+					async: false
+				}).responseText;
+			
 			// Create our data table out of JSON data loaded from server.
 			var data = new google.visualization.DataTable(jsonData);
 			var options = {
-			width: 800, height: 480,
-			title: ‘Company Performance’
+				width: 800, height: 480,
+				title: 'Company Performance'
 			};
-			var chart = new google.visualization.LineChart(document.getElementById(‘chart_div’));
+			
+			var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
 			chart.draw(data, options);
 			}
 			</script>
