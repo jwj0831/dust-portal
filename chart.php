fc655c7a295a38e92058b824b00eb038ca795b78
NFM <jwj0831@gmail.com>
2013-12-03 19:24:55 +0900

    test

--- chart.php
+++ chart.php
@@ -1,24 +1,24 @@
 <?php
 include ("header.php");
 ?>
-			<script>
-				google.load(“visualization”, “1″, {packages:["corechart"]});
-				google.setOnLoadCallback(drawChart);
-				function drawChart() {
-						var jsonData = $.ajax({
-						url: “chart_process.php”,
-						dataType:”json”,
-						async: false
-					}).responseText;
-					// Create our data table out of JSON data loaded from server.
-					var data = new google.visualization.DataTable(jsonData);
-					var options = {
-						width: 800, height: 480,
-						title: ‘Company Performance’
-					};
-					var chart = new google.visualization.LineChart(document.getElementById(‘chart_div’));
-					chart.draw(data, options);
-				}
+			<script type=”text/javascript”>
+			google.load(“visualization”, “1″, {packages:["corechart"]});
+			google.setOnLoadCallback(drawChart);
+			function drawChart() {
+			var jsonData = $.ajax({
+			url: “getData_json.php”,
+			dataType:”json”,
+			async: false
+			}).responseText;
+			// Create our data table out of JSON data loaded from server.
+			var data = new google.visualization.DataTable(jsonData);
+			var options = {
+			width: 800, height: 480,
+			title: ‘Company Performance’
+			};
+			var chart = new google.visualization.LineChart(document.getElementById(‘chart_div’));
+			chart.draw(data, options);
+			}
 			</script>
 			<div id="chart_div" style="width: 900px; height: 500px;"></div>
 		
