41dd3088cd397572d08c1c777e47863e4cd08845
NFM <jwj0831@gmail.com>
2013-12-04 12:02:22 +0900

    update

--- settings.php
+++ settings.php
@@ -18,33 +18,30 @@ include ("header.php");
 ?>
 			<form method="post" role="form" action="./setting_update.php">
 				<div class="form-group">
-			    	<label for="lc">Lower Constant: </label>
-			    	<input type="text" class="form-control" id="lc" name="lc" value="<?php echo $lc;?>">
-			  	</div>
-			  	<div class="form-group">
-			    	<label for="lrc">Lower Constant Frequency: </label>
-			    	<input type="text" class="form-control" id="lrc" name="lrc" value="<?php echo $lrc;?>">
-			  	</div>
-			  	<div class="form-group">
-			    	<label for="mc">Middle Constant: </label>
-			    	<input type="text" class="form-control" id="mc" name="mc" value="<?php echo $mc;?>">
-			  	</div>
-			  	<div class="form-group">
-			    	<label for="mrc">Middle Constant Frequency: </label>
-			    	<input type="text" class="form-control" id="mrc" name="mrc" value="<?php echo $mrc;?>">
-			  	</div>
-			  	<div class="form-group">
-			    	<label for="hc">Higher Constant: </label>
-			    	<input type="text" class="form-control" id="hc" name="hc" value="<?php echo $hc;?>">
-			  	</div>
-			  	<div class="form-group">
-			    	<label for="hrc">Higher Constant Frequency: </label>
-			    	<input type="text" class="form-control" id="hrc" name="hrc" value="<?php echo $hrc;?>">
-			  	</div>
-			  	<div class="form-group">
-			    	<label for="window">Window Size: </label>
+			    	<label for="window">Past Window Size: </label>
 			    	<input type="text" class="form-control" id="window" name="window" value="<?php echo $window;?>">
+			    	<p class="help-block">This number means how many past records are used for deciding IDI.</p>
 			  	</div>
+				<div class="form-group">
+			    	<label for="hc">HRC(Higher Reference Constant): </label>
+			    	<input type="text" class="form-control" id="hc" name="hc" value="<?php echo $hc;?>">
+					<p class="help-block">This reference number is compared with the latest list's figures in past window for counting how many figures are higher than HRC, itself.</p>
+			  	</div>
+			  	<div class="form-group">
+			    	<label for="hrc">RFHRC(Reference Frequency of HRC): </label>
+			    	<input type="text" class="form-control" id="hrc" name="hrc" value="<?php echo $hrc;?>">
+			    	<p class="help-block">The minimum reference number for deciding "severe" index.</p>
+			  	</div>
+			  	<div class="form-group">
+			    	<label for="mc">MRC(Middle Reference Constant): </label>
+			    	<input type="text" class="form-control" id="mc" name="mc" value="<?php echo $mc;?>">
+			    	<p class="help-block">This reference number is compared with the latest list's figures in past window for counting how many figures are higher than MRC, itself.</p>
+			  	</div>
+			  	<div class="form-group">
+			    	<label for="mrc">RFHRC(Reference Frequency of MRC): </label>
+			    	<input type="text" class="form-control" id="mrc" name="mrc" value="<?php echo $mrc;?>">
+			    	<p class="help-block">The minimum reference number for deciding "not bad" index.</p>
+			  	</div>			  	
 			 	 <button type="submit" class="btn btn-default">Submit</button>
 			</form>
 						
