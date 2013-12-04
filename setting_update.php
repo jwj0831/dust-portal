b8003fa5ad565a63d5cee40b2a8546e719bdf7f6
NFM <jwj0831@gmail.com>
2013-12-04 12:36:14 +0900

    up

--- setting_update.php
+++ setting_update.php
@@ -2,9 +2,9 @@
 include ("config.inc.php");
 $window = $_POST['pws'];
 $hc = $_POST['hrc'];
-$hrc = $_POST['rfhrc'];
+$rfhrc = $_POST['rfhrc'];
 $mc = $_POST['mrc'];
-$mrc = $_POST['rfmrc'];
+$rfmrc = $_POST['rfmrc'];
 
 $stmt = $mysqli->prepare("UPDATE dust_conf SET pws = ?, hrc = ?, rfhrc = ?, mrc = ?, rfmrc = ? WHERE id = 1");
 $stmt->bind_param('iiiii', $pws, $hrc, $rfhrc, $mrc, $rfmrc );
