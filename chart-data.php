d87f086b28561ed8fd75e8a0a7a1bc4d38f1fd63
NFM <jwj0831@gmail.com>
2013-12-18 00:23:44 +0900

    q

--- chart-data.php
+++ chart-data.php
@@ -3,7 +3,7 @@ header("Content-type: text/json");
 
 include("config.inc.php"); //include config file
 
-$result = $mysqli->query("SELECT UNIX_TIMESTAMP( timestamp ), raw_data, idi_data FROM dust_data ORDER BY id DESC LIMIT 1");
+$result = $mysqli->query("SELECT UNIX_TIMESTAMP( timestamp ) * 1000, raw_data, idi_data FROM dust_data ORDER BY id DESC LIMIT 1");
 $row = $result->fetch_row();
 /*
 $data['cols'][] = array('type' => 'string', 'label' => 'timestamp');
@@ -15,11 +15,10 @@ for($i=49; $i >= 0 ; $i--){
 	$data['rows'][] = array('c' => array( array('v' => $row['timestamp']), array('v' => $row['raw_data']),array('v' => $row['idi_data']) ) );
 }
 */
-$x = (float) $row[0];
+$x = (in) $row[0];
 $y = (float) $row[1];
 $z = (int) $row[2];
-$jstime = $x * 1000;
-$data = array($jstime, $y, $z);
+$data = array($x, $y, $z);
 
 echo json_encode($data);
 
